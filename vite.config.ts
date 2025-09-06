4484fb1ae6327172e67fd671bb92779667c42a93
Dan <shumilov.dss@gmail.com>
2025-09-06 17:56:20 +0500

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-// vite.config.ts (или .js)1
+// vite.config.ts (или .js)
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
@@ -7,8 +7,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", ""); // читает .env, .env.production и т.п.
 
   return {
-    // ВАЖНО: укажи точное имя репозитория с учётом регистра
-    // например: base: "/FapCollection/"
     base: "/xcollection/",
 
     plugins: [react()],
